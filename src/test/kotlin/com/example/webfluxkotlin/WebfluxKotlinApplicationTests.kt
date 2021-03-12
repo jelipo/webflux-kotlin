078697e9c40021165e35df7320cd31ea9d6c3949
jelipo <me@jelipo.com>
2021-03-12 17:40:22 +0800

    Initial commit

---
+++ src/test/kotlin/com/example/webfluxkotlin/WebfluxKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.webfluxkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
