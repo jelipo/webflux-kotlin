38a8d6ea0867f41e5e6499d20a487eb20edf86c6
jelipo <me@jelipo.com>
2021-03-14 23:12:29 +0800

    try async

--- src/main/kotlin/com/example/webfluxkotlin/ctrl/Test.kt
+++ src/main/kotlin/com/example/webfluxkotlin/ctrl/Test.kt
@@ -1,6 +1,10 @@
 package com.example.webfluxkotlin.ctrl
 
 import com.example.webfluxkotlin.service.TestService
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -18,7 +22,9 @@ class Test(
     @GetMapping("test")
     suspend fun test(): String {
         //println("Current thread name: " + Thread.currentThread().name)
-        val request = testService.request()
-        return request
+        val async = GlobalScope.async {
+            testService.request()
+        }
+        return async.await()
     }
 }
--- src/main/kotlin/com/example/webfluxkotlin/service/TestService.kt
+++ src/main/kotlin/com/example/webfluxkotlin/service/TestService.kt
@@ -1,5 +1,6 @@
 package com.example.webfluxkotlin.service
 
+import kotlinx.coroutines.withTimeout
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
@@ -10,11 +11,11 @@ class TestService(
     private val webClient: WebClient
 ) {
 
-
     suspend fun request(): String {
         val body = webClient.get().uri("https://hectorstatic.baidu.com/cd37ed75a9387c5b.js").awaitExchange {
             it.awaitBody(String::class)
         }
+
         return body
     }
 
